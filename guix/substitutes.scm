a117edea1255951626b11dc2bb411c874c0b7824
starmun-0010 <abdemuqsit@hotmail.com>
2022-10-20 09:34:21 +0500

    Added custom substitutes

---
+++ guix/substitutes.scm
@@ -0,0 +1,10 @@
+(operating-system
+  (services (modify-services %desktop-services
+             (guix-service-type config => (guix-configuration
+               (inherit config)
+               (substitute-urls
+                (append (list "https://substitutes.nonguix.org")
+                  %default-substitute-urls))
+               (authorized-keys
+                (append (list (local-file "signing-key.pub"))
+                  %default-authorized-guix-keys))))))
